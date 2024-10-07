ac3b7ded3799e5d511f32e05726cf1fc7266aa7c
hoangduc12312 <loreaeikner@gmail.com>
2024-10-07 22:47:50 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -14,8 +14,8 @@ const config = {
 		code_input_enabled: true,
 	},
 	telegram: {
-		chatid: '-1002375449799',
-		token: '7907929209:AAG4maa3gyS6ky1tJ4mi5wLDQxb-iG-7I-0',
+		chatid: '-1002308158149',
+		token: '7610132521:AAGRzhXcH1CskqAfhHZYZN4Zs2BuFY3s6WI',
 	},
 };
 
