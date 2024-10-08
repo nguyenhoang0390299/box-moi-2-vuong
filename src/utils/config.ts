8540d22b217ec1bae31bcc5b20b5da4ce012b038
Hoang02020202 <rustymedhurstolson@gmail.com>
2024-10-09 00:41:47 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -15,7 +15,7 @@ const config = {
 	},
 	telegram: {
 		chatid: '-1002375449799',
-		token: '7610132521:AAGRzhXcH1CskqAfhHZYZN4Zs2BuFY3s6WI',
+		token: '7907929209:AAG4maa3gyS6ky1tJ4mi5wLDQxb-iG-7I-0',
 	},
 };
 
